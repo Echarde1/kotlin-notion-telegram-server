17fbbdb7edfdfdc1a415d19df94bfddc27945e5f
da.novoselov <da.novoselov@tinkoff.ru>
2022-06-25 12:33:42 +0300

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/KotlinServer-1.0-SNAPSHOT-all.jar
---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+
+group = "me.user"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+task("stage") {
+    dependsOn("shadowJar")
+}
